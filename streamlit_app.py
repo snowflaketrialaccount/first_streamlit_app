fb75751a5d618686b94d13308d49584a289c58ad
trial account <114595803+snowflaketrialaccount@users.noreply.github.com>
2022-09-29 12:41:53 +0100

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 if streamlit.button('Add a Fruit to the List'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         back_from_function = insert_row_snowflake(add_my_fruit)
-        streamlit.text(nack_from_function)
+        streamlit.text(back_from_function)
 
 streamlit.write('Thanks for adding ', add_my_fruit)
 
